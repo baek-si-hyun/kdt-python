00db1b2700c8cd26c0ff75e2919020dd51b936b3
baek-si-hyun <qortlgus100@gmail.com>
2024-01-18 17:21:54 +0900

    20240118 relation

---
+++ s_mysql/task/relation/relation.sql
@@ -0,0 +1,39 @@
+# 숙제
+create table tbl_client(
+    email varchar(255) primary key,
+    password varchar(255) not null,
+    name varchar(255) not null
+);
+
+create table tbl_office(
+    id bigint auto_increment primary key,
+    name varchar(255) not null,
+    location varchar(255) not null
+);
+
+create table tbl_conference_room(
+    id bigint auto_increment primary key,
+    office_id bigint not null,
+    constraint fk_conference_room_office foreign key(office_id)
+                                references tbl_office(id)
+);
+
+create table tbl_part_time(
+    id bigint auto_increment primary key,
+    time time not null,
+    conference_room_id bigint not null,
+    constraint fk_part_time_conference_room foreign key(conference_room_id)
+                                references tbl_conference_room(id)
+);
+
+create table tbl_reservation(
+    id bigint auto_increment primary key,
+    time time not null,
+    created_date datetime default (current_timestamp),
+    client_email varchar(255) not null,
+    conference_room_id bigint not null,
+    constraint fk_reservation_client foreign key (client_email)
+                            references tbl_client(email),
+    constraint fk_reservation_conference_room foreign key (conference_room_id)
+                            references tbl_conference_room(id)
+);

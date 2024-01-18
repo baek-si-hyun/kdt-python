c9aad39903ab0575a790fd33e758a0dd75e350c1
baek-si-hyun <qortlgus100@gmail.com>
2024-01-18 13:43:45 +0900

    20240117 mysql

---
+++ s_mysql/connection_module.py
@@ -0,0 +1,31 @@
+import pymysql
+from pymysql.cursors import Cursor
+
+
+def connect():
+    conn = pymysql.connect(host='15.165.77.109', user='mysql', password='1234', db='test', charset='utf8',
+                           autocommit=False)
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    return conn, cursor
+
+
+def execute(crud):
+    result = None
+
+    def manage(*args):
+        nonlocal result
+        conn, cursor = connect()
+        try:
+            result = crud(cursor, *args)
+            conn.commit()
+        except Exception as e:
+            print(e.__str__())
+            conn.rollback()
+
+        finally:
+            cursor.close()
+            conn.close()
+
+        return result
+
+    return manage
